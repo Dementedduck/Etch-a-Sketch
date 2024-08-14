93aca64ce53a2a6ce9fc85de869c64ea21f3cacd
Dementedduck <155681526+Dementedduck@users.noreply.github.com>
2024-08-14 20:14:04 +0100

    Add drawing functionality Add various buttons Add size select

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
-let container = document.querySelector(".container");
-container.style.gridTemplateColumns = "repeat(16 , 1fr)";
-container.style.gridTemplateRows = "repeat(16 , 1fr)";
+const sizeButton = document.querySelector(".size");
+const colourButton = document.querySelector(".colour");
+const precisionButton = document.querySelector(".precision");
+const clearButton = document.querySelector(".clear");
 
-let gridNumber = 16 * 16;
+function create(size) {
+  let container = document.querySelector(".container");
+  let allSquares = container.querySelectorAll("div");
+  allSquares.forEach((div) => div.remove());
+  container.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
-for (let i = 0; i < gridNumber; i++) {
+  let gridNumber = size * size;
+
+  for (let i = 0; i < gridNumber; i++) {
     let divs = document.createElement("div");
-    divs.className = "squares";
+    divs.addEventListener("mouseover", () => {
+      divs.style.backgroundColor = "black";
+    });
     container.insertAdjacentElement("beforeend", divs);
+  }
 }
 
+function clear() {
+    let allSquares = container.querySelectorAll("div");
+    allSquares.forEach((div) => div.remove());
+}
+
+create(16);
+
+function changeSize(input) {
+  if (input >= 2 && input <= 100) {
+    create(input);
+  } else {
+    alert("Input a valid number!");
+  }
+}
+
+sizeButton.addEventListener("click", function () {
+  let user = prompt(
+    "Choose your size! (The number you choose will be the width and height of the square, eg: 16 would be 16x16). ONLY 2 - 100 are allowed!!!"
+  );
+  changeSize(user);
+});
