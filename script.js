7ad9d6c3373ee85a167e3996e8d09d33512ce436
Dementedduck <155681526+Dementedduck@users.noreply.github.com>
2024-08-14 21:11:07 +0100

    Add colour changing Add better size selection Add clearing

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+let colour = "black";
+
 const sizeButton = document.querySelector(".size");
 const colourButton = document.querySelector(".colour");
 const precisionButton = document.querySelector(".precision");
 const clearButton = document.querySelector(".clear");
-
+const container = document.querySelector(".container");
 function create(size) {
-  let container = document.querySelector(".container");
   let allSquares = container.querySelectorAll("div");
   allSquares.forEach((div) => div.remove());
   container.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
@@ -14,19 +15,18 @@ function create(size) {
 
   for (let i = 0; i < gridNumber; i++) {
     let divs = document.createElement("div");
-    divs.addEventListener("mouseover", () => {
-      divs.style.backgroundColor = "black";
-    });
+    divs.addEventListener("mouseover", colourSquare);
     container.insertAdjacentElement("beforeend", divs);
   }
 }
 
-function clear() {
-    let allSquares = container.querySelectorAll("div");
-    allSquares.forEach((div) => div.remove());
-}
-
 create(16);
+
+function clear() {
+  let allSquares = container.querySelectorAll("div");
+  allSquares.forEach((div) => div.remove());
+  create(16);
+}
 
 function changeSize(input) {
   if (input >= 2 && input <= 100) {
@@ -36,9 +36,28 @@ function changeSize(input) {
   }
 }
 
+function colourSquare() {
+  this.style.backgroundColor = colour;
+}
+
+function chooseColour(input) {
+  colour = input;
+}
+
+colourButton.addEventListener("click", function () {
+  let answer = prompt(
+    "Choose your colour! (Only hexcode and basic values are accepted)"
+  );
+  chooseColour(answer);
+});
+
 sizeButton.addEventListener("click", function () {
   let user = prompt(
     "Choose your size! (The number you choose will be the width and height of the square, eg: 16 would be 16x16). ONLY 2 - 100 are allowed!!!"
   );
   changeSize(user);
+});
+
+clearButton.addEventListener("click", function () {
+  clear();
 });
